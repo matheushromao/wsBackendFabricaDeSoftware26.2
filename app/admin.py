46ee07ff55cda472720a08b06669cbb212a9cde7
Matheus Romão <matheus_henrique_9@hotmail.com>
2026-08-29 02:22:16 -0300

    feat: registra models Time e AtletaEscalado no admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import AtletaEscalado, Time
 
 # Register your models here.
+
+@admin.register(Time)
+class TimeAdmin(admin.ModelAdmin):
+    list_display = ("nome", "usuario", "criado_em")
+    search_fields = ("nome", "usuario_username")
+    
+    
+@admin.register(AtletaEscalado)
+class AtletaEscaladoAdmin(admin.ModelAdmin):
+    list_display = ("apelido", "clube", "posicao", "preco_cartoletas", "time")
+    list_filter = ("posicao", "clube")
+    search_fields = ("apelido", "clube")
