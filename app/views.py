0f9e86def0671ea6d8d0cf8dde47015c49736131
Matheus Romão <matheus_henrique_9@hotmail.com>
2026-08-29 02:52:02 -0300

    feat adiciona autenticacao com cadastro, login e logout

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect ,render
 
 # Create your views here.
+
+def cadastro(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            usuario = form.save()
+            login(request, usuario)
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+        
+    return render(request, "app/cadastro.html", {"form": form})
